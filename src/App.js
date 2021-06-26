e013679377fe84e96658102fde098e43d157f954
sunnymishra <sunny.mishra0389@gmail.com>
2021-06-26 16:28:43 +0530

    feature-branch created

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. This is a changed text in feature branch.
         </a>
       </header>
     </div>
